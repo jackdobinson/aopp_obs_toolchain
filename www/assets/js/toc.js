9709e882025f3cf0b88dc0cb573e99558346d5ca
jackdobinson <jack.dobinson@physics.ox.ac.uk>
2024-08-06 16:51:44 +0100

    ghpages toc from script

---
+++ www/assets/js/toc.js
@@ -0,0 +1,101 @@
+"use strict"
+
+
+export class TableOfContents{
+
+
+	static text_to_anchor(text){
+		return text.toLowerCase()
+			.replace(' ','-')
+			.replace('/', '-back-slash-')
+	}
+
+	static from_headings_in(container){
+		elements = container.querySelectorAll("h1, h2, h3, h4, h5, h6")
+		return TableOfContents(
+			toc_parent_element.ownerDocument,
+			Map.groupBy(elements, (e)=>{return parseInt(e.nodeName[1])})
+		)
+	}
+
+	constructor(document, toc_level_to_element_map, list_type='ul', item_type='li'){
+		this.doc = document
+		this.list_type = list_type
+		this.item_type = item_type
+		
+		this.toc_level = 0
+		this.toc_level_stack = [this.doc.createElement('div')]
+		this.current_level_element.setAttribute('class', 'table-of-contents')
+		
+		
+		for (const [level, element] of toc_level_to_element_map.entries()){
+			this.process(level, element)
+		}
+	
+	}
+	
+	as_child_of(html_parent_element){
+		for(const child of this.toc_level_stack[0]){
+			html_parent_element.appendChild(child) // child.cloneNode()
+		}
+	}
+	
+	get current_level_element(){
+		return this.toc_level_stack.length==0 ? null : this.toc_level_stack.at(-1)
+	}
+	
+	process(level, element){
+		while(this.toc_level < level){
+			this.toc_level += 1
+			this.push()
+		}
+		while (this.toc_level > level){
+			this.toc_level -= 1
+			this.pop()
+		}
+		this.add_entry_for(element)
+	}
+	
+	new_list_container() {
+		const list = this.doc.createElement(this.list_type)
+		list.setAttribute('class', `level-${this.toc_level} empty`)
+		this.current_level_element.appendChild(list)
+		return list
+	}
+	
+	new_list_item(anchor, text){
+		const item = this.doc.createElement(this.item_type)
+		const a = this.doc.createElement('a')
+		a.setAttribute('href', `#${anchor}`)
+		a.textContent = text
+		item.appendChild(a)
+		return item
+	}
+	
+	
+	add_entry_for(element){
+		const text = element.textContent
+		
+		var anchor = element.getAttribute('id')
+		if (anchor === null){
+			anchor = TableOfContents.text_to_anchor(text)
+			element.setAttribute('id', anchor)
+		}
+		
+		this.current_level_element.addChild(this.new_list_item(anchor, text))
+		this.current_level_element.setAttribute('class',
+			this.current_level_element.getAttribute('class').replace(' empty', '')
+		)
+	}
+	
+	push(){
+		this.toc_level_stack.push(this.new_list_container())
+	}
+	
+	pop(){
+		this.toc_level_stack.pop()
+	}
+	
+	
+
+}
