42d5ada4392bf86560e28eea8fe1bfe8a0ed2aa2
Jack Dobinson <jack.dobinson@physics.ox.ac.uk>
2024-12-12 15:43:57 +0000

    adding title to page

--- www/webapp/fetch_webapp_files.sh
+++ www/webapp/fetch_webapp_files.sh
@@ -35,7 +35,7 @@ cp -r ${WEBAPP_DIR}/*.js ${WEBAPP_DIR}/js_modules ${THIS_DIR}/
 # Add link back to home page
 
 find_1='<a id="link-to-home"></a>'
-replace_1='<nav class="top-menu"><a id="link-to-home" href="https://jackdobinson.github.io/aopp_obs_toolchain/index.html">Home</a></nav>'
+replace_1='<nav class="top-menu"><a id="link-to-home" href="https://jackdobinson.github.io/aopp_obs_toolchain/index.html">Home</a></nav><h1>Deconv Tool Web Application</h1>'
 
 sed -i -e "s#${find_1}#${replace_1}#g" ${THIS_DIR}/index.html
 
