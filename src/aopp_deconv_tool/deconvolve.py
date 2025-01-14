fixed a fail state where normalising the PSF can lead to the PSF and SCI input files having different non-celestial-axis shapes, which would mean the deconvolution did not work correctly.
Added a test to catch if SCI and PSF inputs to deconvolution have different non-celestial-axis shapes.

src/aopp_deconv_tool/deconvolve.py
@@ -318,6 +318,7 @@ def display_process_progress():
 		except OSError as e:
 			#print('queue stopping')
 			#print(e)
+			_lgr.error(f"Queue stopped due to error: {e}")
 			stop = True
 			break
 		
@@ -335,13 +336,14 @@ def display_process_progress():
 		
 		if group_index is None and msg is None:
 			#print('queue stopping')
+			_lgr.info("Queue stopping as got 'stop' signal (None,None)")
 			stop = True
 			break
 		
 		#print('SIX')
 		
 		if msg is None:
-			#print(f'msg is "None", deleting state tracker for {group_index=}')
+			_lgr.info(f'msg is "None", deleting state tracker for {group_index=}')
 			if group_index in most_recent_msg:
 				del most_recent_msg[group_index]
 			if group_index in display_msg:
@@ -461,11 +463,18 @@ def run(
 		original_data_type=obs_data.dtype
 		
 		group_shape = tuple(s for x, s in enumerate(obs_data.shape) if x not in obs_fits_spec.axes['CELESTIAL'])
+		psf_group_shape = tuple(s for x, s in enumerate(psf_data.shape) if x not in psf_fits_spec.axes['CELESTIAL'])
+		
+		assert len(group_shape) == len(psf_group_shape), f"After slicing, SCI and PSF observations MUST have the same number of dimensions. Currently SCI {group_shape} PSF {psf_group_shape}."
+		assert all(group_shape[i] == psf_group_shape[i] for i in range(len(group_shape))), f"After slicing, SCI and PSF observations MUST have the same shape. Currently SCI {group_shape} PSF {psf_group_shape}."
+		
+		
 		idx_result = np.full(group_shape, dtype=object, fill_value=dict())
 		
 		_lgr.debug(f'{obs_data.shape=}')
 		_lgr.debug(f'{psf_data.shape=}')
 		_lgr.debug(f'{original_data_type=}')
+		
 		
 		# Create holders for deconvolution products
 		deconv_components = np.full_like(obs_data, np.nan)
@@ -525,13 +534,14 @@ def run(
 				
 				futures.append(executor.submit(deconv_process, group_index, obs_idx, deconvolver, processed_obs, normed_psf))
 			
-			print(f'Waiting for processes...')
+			
+			print(f'Waiting for {len(futures)} processes...')
 			for future in as_completed(futures):
 				if not future.done():
 					_lgr.error(f'Deconv process {future} not completed, something went wrong')
 					continue
 				if future.cancelled():
-					_lgr.warn(f'Deconv process {fugure} cancelled, skipping result...')
+					_lgr.warn(f'Deconv process {future} cancelled, skipping result...')
 					continue
 				
 				_lgr.info(f'Process {group_index} ended')

src/aopp_deconv_tool/psf_normalise.py
@@ -29,6 +29,20 @@ import aopp_deconv_tool.cfg.logs
 _lgr = aopp_deconv_tool.cfg.logs.get_logger_at_level(__name__, 'WARN')
 
 
+def embed_in_nan_array(
+		data, 
+		embed_slice, 
+		big_shape, 
+		conform_axes # axes to conform to 'big_shape'
+	):
+
+	wanted_shape = tuple(big_shape[i] if (i not in conform_axes) else data.shape[i] for i in range(data.ndim))
+	big_array = np.full(wanted_shape, np.nan)
+	big_array[*embed_slice] = data
+	return big_array
+	
+	
+
 def run(
 		fits_spec : aph.fits.specifier.FitsSpecifier,
 		output_path : Path,
@@ -138,21 +152,21 @@ def run(
 	
 	hdus.append(fits.PrimaryHDU(
 		header = hdr,
-		data = normalised_data.astype(original_data_type)
+		data = embed_in_nan_array(normalised_data, fits_spec.slices, data_hdu.shape, fits_spec.axes['CELESTIAL']).astype(original_data_type)
 	))
 	hdus.append(fits.ImageHDU(
 		header = hdr,
-		data = roi_mask.astype(int),
+		data = embed_in_nan_array(roi_mask, fits_spec.slices, data_hdu.shape, fits_spec.axes['CELESTIAL']).astype(int),
 		name = 'ROI_MASK'
 	))
 	hdus.append(fits.ImageHDU(
 		header = hdr,
-		data = background_mask.astype(int),
+		data = embed_in_nan_array(background_mask, fits_spec.slices, data_hdu.shape, fits_spec.axes['CELESTIAL']).astype(int),
 		name = 'BACKGROUND_MASK'
 	))
 	hdus.append(fits.ImageHDU(
 		header = hdr,
-		data = outlier_mask.astype(int),
+		data = embed_in_nan_array(outlier_mask, fits_spec.slices, data_hdu.shape, fits_spec.axes['CELESTIAL']).astype(int),
 		name = 'OUTLIER_MASK'
 	))
 	hdus.append(fits.BinTableHDU.from_columns(